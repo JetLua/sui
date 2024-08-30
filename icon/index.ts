0a031af13c2e97dcd43b445fe0e59aeac08327cd
JetLu <i@lufei.im>
2024-08-31 01:40:15 +0800

    update

--- icon/index.ts
+++ icon/index.ts
@@ -1,3 +1,4 @@
 export {default as Plus} from './Plus.svelte'
+export {default as Check} from './Check.svelte'
 export {default as FolderPlus} from './FolderPlus.svelte'
 export {default as FileUpload} from './FileUpload.svelte'
