b45a2237d1e53a4b703cb828420e810004f782dc
JetLu <i@lufei.im>
2024-09-18 19:47:26 +0800

    update

---
+++ icon/Lock.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import clsx from 'clsx'
+import type {SVGAttributes} from 'svelte/elements'
+
+interface Props extends SVGAttributes<SVGSVGElement> {
+
+}
+
+let {class: _class, ...props}: Props = $props()
+
+_class = clsx('stroke-[var(--stroke,#999)] fill-[var(--fill,transparent)]', _class)
+</script>
+
+<svg
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  stroke-width="1"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+  class={_class} {...props}>
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M5 13a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v6a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-6z" />
+  <path d="M11 16a1 1 0 1 0 2 0a1 1 0 0 0 -2 0" />
+  <path d="M8 11v-4a4 4 0 1 1 8 0v4" />
+</svg>
