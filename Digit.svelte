f48507f2a0d513488e7b816989c9e7763d55a9df
JetLu <i@lufei.im>
2024-10-13 00:26:15 +0800

    update

--- Digit.svelte
+++ Digit.svelte
@@ -10,25 +10,42 @@
 
   const {value, ...props}: Props = $props()
 
-  const snap = $state({
-    value: 0
-  })
+  // const snap = $state({
+  //   value: 0
+  // })
 
-  function tick(value: number) {
-    animate({
-      from: snap.value,
-      to: value,
-      duration: 3e2,
-      onUpdate: v => {
-        snap.value = v | 0
-      }
-    })
-  }
+  // function tick(value: number) {
+  //   animate({
+  //     from: snap.value,
+  //     to: value,
+  //     duration: 3e2,
+  //     onUpdate: v => {
+  //       snap.value = v | 0
+  //     }
+  //   })
+  // }
 
-  $effect(() => {
-    const v = value
-    untrack(() => tick(v))
-  })
+  // $effect(() => {
+  //   const v = value
+  //   untrack(() => tick(v))
+  // })
 </script>
 
-<span {...props}>{snap.value}</span>
+<span {...props} style:--num={value}></span>
+
+<style lang="scss">
+  @property --num {
+    syntax: '<integer>';
+    initial-value: 0;
+    inherits: false;
+  }
+
+  span {
+    transition: --num 1s;
+    counter-reset: num var(--num);
+  }
+
+  span::after {
+    content: counter(num);
+  }
+</style>
