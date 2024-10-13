95a9290f764b34731d1ebe1afaec1aa159190b44
JetLu <i@lufei.im>
2024-10-13 17:25:33 +0800

    update

---
+++ icon/CircleD.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import clsx from 'clsx'
+import type {SVGAttributes} from 'svelte/elements'
+
+interface Props extends SVGAttributes<SVGSVGElement> {
+
+}
+
+let {class: _class, ...props}: Props = $props()
+
+_class = clsx('stroke-[var(--stroke,#999)] fill-[var(--fill,transparent)]', _class)
+</script>
+
+<svg
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  stroke-width="1"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+  class={_class} {...props}>
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"/>
+  <path d="M10 8v8h2a2 2 0 0 0 2 -2v-4a2 2 0 0 0 -2 -2h-2z"/>
+</svg>
